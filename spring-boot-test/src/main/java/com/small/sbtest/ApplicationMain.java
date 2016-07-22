44c4810cd4fe495b9ad139622e829c759e9aca9b
changwentao <changwenwen@126.com>
2016-07-22 18:05:17 +0800

    fisrt

---
+++ spring-boot-test/src/main/java/com/small/sbtest/ApplicationMain.java
@@ -0,0 +1,23 @@
+package com.small.sbtest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class ApplicationMain {
+	private static Logger logger = LoggerFactory.getLogger(ApplicationMain.class);
+
+	private static String APP_NAME = "我的app";
+
+	public static void main(String[] args) {
+		logger.debug("starting app：" + APP_NAME);
+		SpringApplication.run(ApplicationMain.class);
+		logger.debug(APP_NAME + " started success");
+	}
+}
